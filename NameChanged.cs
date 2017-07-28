2279d313ade7ce1936a9dc9b454f85ae7e522f7c
eshankarora7291 <eshank.arora@ge.com>
2017-07-28 14:30:07 +0530

    new file

---
+++ NameChanged.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testapp
+{
+    public delegate void NameChanged(string old_name, string new_name);
+}
